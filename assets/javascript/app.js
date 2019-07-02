6684bda0a5ce92f8ad6633f64d39dfe85ac338a3
hpraveen <hariharanster@gmail.com>
2019-07-02 01:46:20 -0400

    attempted and no attempt logic is in place

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,76 +1,152 @@
-    
-    
-    //loading first question
-    var i;
-    var quiz=[];
-    var Count;
-    var imageChoice;
-    
-    var correct=0;
-    var missed =0;
-    var attempted = 0;
-    
-    var rightAns;
-    var intervalTimer;
-    var delayButtonAlert;
-    var newQuest;
-    var ansAttempt;
-    
-    
-    
-    quizBuild();        
+$(document).ready(function(){
+//////////////////////////////////////////////////////////////////////////////////////////////
+//variable declaration
+var i;
+var quiz = [];
+var Count;
+var imageChoice;
+var question;
+var ans;
 
-    function quizElements(question,choice1,choice2,choice3,choice4,ans,imageURL,attempted){
-    
-        this.question = question;
-        this.choice1   = choice1;
-        this.choice2   = choice2;
-        this.choice3   = choice3;
-        this.choice4   = choice4;
-        this.ans       = ans;
-        this.imageURL  = imageURL;
-        this.attempted = attempted;
+var correct = 0;
+var missed = 0;
+var attempted = 0;
+
+var rightAns;
+var intervalTimer;
+var delayButtonAlert;
+var newQuest;
+var ansAttempt;
+
+
+quizBuild();
+i = 0;    
+hideStuff();
+
+function quizConstructor(question, choice1, choice2, choice3, choice4, ans, imageURL, attempted) {
+
+    this.question = question;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+    this.choice3 = choice3;
+    this.choice4 = choice4;
+    this.ans = ans;
+    this.imageURL = imageURL;
+    this.attempted = attempted;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+function quizBuild() {
+
+
+    quiz[0] = new quizConstructor('type of galaxy is the most common in the universe','Elliptical galaxies', 'Spherical galaxies', 'Oblong galaxies', 'Helical galaxies', 1, "https://tenor.com/view/galaxy-universe-space-astronaut-moon-gif-13397473.gif", false);
+    quiz[1] = new quizConstructor('is the coldest place in the universe','Boomerang Nebula', 'Eskimo Nebula', 'Helix Nebula', 'Lemon Slice Nebula', 1, 'https://media2.giphy.com/media/3og0INtldac8gncQO4/giphy.gif?cid=790b76115d1acfff6c3452754d47f0d4&rid=giphy.gif', false);
+    quiz[2] = new quizConstructor('percent of the universe is dark matter','96%', '48%', '27%', '83%', 3, 'https://66.media.tumblr.com/188ed820349f02486f8768c7011167f0/tumblr_ncol0igSYj1rnq3cto4_500.gif', false);
+    quiz[3] = new quizConstructor('is the largest planet in our solar system','Saturn', 'Uranus', 'Earth', 'Jupiter', 4, 'https://i.imgur.com/Kmv7vbJ.gif', false);
+    quiz[4] = new quizConstructor('is the smallest planet in our solar system','Venus', 'Mars', 'Earth', 'Mercury', 4, 'https://d2t1xqejof9utc.cloudfront.net/screenshots/pics/9dc18a44a92486b24c4ee7be1e51b959/large.gif', false);
+    return quiz;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+$("#new-question").on("click", displayNewQuestion);
+
+function displayNewQuestion() {
+
+    //hide the button after the first click;  new questions
+    //will automatically display with timers
+
+    if (i > 0) { clearTimeout(newQuest); }
+
+    $("#new-question").hide();
+    hideStuff();
+
+    //send question and options info to screen
+
+    ansAttempt = false;
+    console.log("New question " + i + " will display now");
+    quizWrite();
+
+    //Initialize count & display
+    Count = 30;
+    intervalTimer = setInterval(countDown, 1000)
+
+    //take this path if time expires..
+    delayButtonAlert = setTimeout(notAttempted, 30000)
+}
+
+$(document).on("click",".answer",Attempted);
+
+function countDown() {
+    Count -= 1;
+    $("#seconds-count").html('<h3> You have ' + Count + " seconds left</h3> ")
+    return Count;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+function notAttempted() {
+
+    //this executes after 30 seconds have passed
+    //clear timers when this is called
+    if (ansAttempt != true) {
+
+        clearTimeout(delayButtonAlert);
+        clearTimeout(intervalTimer);
+
+        //other two stats will not change
+
+        missed += 1;
+
+        $(".stats").show();
+        displayStats();
+
+        displayAnsImg();
     }
+    else {
+        return
+    }
+}
 
-    function quizBuild(){
-    
-     
-            quiz[0] = new quizElements('Elliptical galaxies','Spherical galaxies','Oblong galaxies','Helical galaxies','Double helix galaxies',1,"https://tenor.com/view/galaxy-universe-space-astronaut-moon-gif-13397473.gif",false);
-            quiz[1] = new quizElements('The Boomerang Nebula','Eskimo Nebula','Helix Nebula','Lemon Slice Nebula','Oyster Nebula',1,'https://media2.giphy.com/media/3og0INtldac8gncQO4/giphy.gif?cid=790b76115d1acfff6c3452754d47f0d4&rid=giphy.gif',false);
-            quiz[2] = new quizElements('96%','48%','27%','83%','77%',3,'https://66.media.tumblr.com/188ed820349f02486f8768c7011167f0/tumblr_ncol0igSYj1rnq3cto4_500.gif',false);
-            
+///////////////////////////////////////////////////////////////////////////////
+
+function Attempted(){
         
-            return quiz
+    //this executes after an answer was attempted
+    //clear timers when this is called
+    
+    clearTimeout(delayButtonAlert);
+    clearTimeout(intervalTimer);
+
+    // this function is called if an answer is
+    // attempted before the timeout
+
+    ansAttempt = true;
+    $("#message").show();
+    
+
+    userChoice = parseInt($(this).val());
+    //console.log(userChoice, i);
+
+    attempted += 1;
+
+    if (userChoice == quiz[i].ans){
+        $("#message").html('Correct Answer!');
+        correct += 1;
         }
-
-        $("#new-question").on("click",displayNewQuestion);
-    
-    function displayNewQuestion(){
-    
-        //hide the button after the first click;  new questions
-        //will automatically display with timers
-    
-        if (i > 0) {clearTimeout(newQuest);}
-    
-        $("#new-question").hide();
-        hideStuff();
-    
-        //send question and options info to screen
-    
-        ansAttempt = false;
-        console.log("New question " + i +  " will display now");
-        quizWrite();
-    
-        //Initialize count & display
-        Count = 20;
-        intervalTimer = setInterval(countDown,1000)
-    
-        //take this path if time expires..
-        delayButtonAlert = setTimeout(notAttempted,1000)   
+    else {
+        $("#message").html('Incorrect Answer!');   
+        missed += 1;
     }
 
-    function countDown(){
-        Count -= 1;
-        $("#seconds-count").html('<h3> You have '+ Count + " seconds left</h3> ") 
-        return Count;
-    }
+    $(".stats").show();
+    displayStats();
+
+    //call the function that displays the answer and gif
+
+    displayAnsImg();
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+}) 
